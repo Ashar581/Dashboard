14ba883dc3a6150673756c1adb4b193e9a578aac
Ashar581 <asharshahab@gmail.com>
2024-01-10 02:54:55 +0530

    Initial commit

--- src/main/java/com/Dashboard/Dashboard/DashboardApplication.java
+++ src/main/java/com/Dashboard/Dashboard/DashboardApplication.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.util.List;
 
 @SpringBootApplication
-public class DashboardApplication{
+public class DashboardApplication implements CommandLineRunner{
 
 	public DashboardApplication() throws CsvValidationException, IOException {
 	}
@@ -24,18 +24,18 @@ public class DashboardApplication{
 		SpringApplication.run(DashboardApplication.class, args);
 	}
 
-//	@Autowired
-//	private DataRepository dataRepository;
-//
-//	DataReceiver dataReceiver = new DataReceiver();
-//
-//	List<Data> dataList = readCsv.addUser();
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//
-//		dataRepository.saveAll(dataList);
-//
-//	}
+	@Autowired
+	private DataRepository dataRepository;
+
+	DataReceiver dataReceiver = new DataReceiver();
+
+	List<Data> dataList = readCsv.addUser();
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		dataRepository.saveAll(dataList);
+
+	}
 
 }
