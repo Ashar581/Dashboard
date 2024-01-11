bb016cb6ae92c0025a99df46379bfec4814c530a
Ashar581 <asharshahab@gmail.com>
2024-01-11 23:04:16 +0530

    Initial commit

--- src/main/java/com/Dashboard/Dashboard/readCSV/readCsv.java
+++ src/main/java/com/Dashboard/Dashboard/readCSV/readCsv.java
@@ -38,7 +38,7 @@ public class readCsv {
         String title;
         String likelihood;
 
-        CSVReader csvReader = new CSVReader(new FileReader("src/Data.csv"));
+        CSVReader csvReader = new CSVReader(new FileReader("src/main/resources/Data.csv"));
 
         String [] readLine;
 
